748e1169fe943b3a3f14a56fa4a067d2d52107b3
benlayer291 <benlayer291@gmail.com>
2015-12-12 12:21:37 +0000

    js written to arrange shapes on game grid and to load players shapes. Upon click of gridsquare, analysed whether correct or not and green or red shown accordingly. Player shapes updated after each correct move

--- game-logic/js/app.js
+++ game-logic/js/app.js
@@ -4,7 +4,63 @@ $(function(){
 })
 
 function initialize(){
-  $('.gridsquare').on('click', function(){
-    console.log('click');
-  })
+  setupGameGrid();
+  setupPlayerGrid(6);
+  $('.game-gridsquare').on('click', playGame)
 }
+
+// Need to assign shapes to game-gridsquares
+
+var shapes       = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+var gameGridsize = Math.pow(3,2);
+var gameGrid     = [];
+var playerGrid   = [];
+var playerScore  = 0;
+
+function setupGameGrid(){
+
+  for (var i = 0; i < gameGridsize; i++) {
+
+    var shape = shapes[Math.floor(Math.random()*shapes.length)];
+    var shapeIndex = shapes.indexOf(shape);
+    
+    gameGrid.push(shape);
+    shapes.splice(shapeIndex, 1);
+  }
+
+  for (var i = 0; i < gameGridsize; i++) {
+    $('#game-gridsquare-'+i).html(gameGrid[i]);
+  }
+  shapes = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+  return gameGrid;
+}
+
+function setupPlayerGrid(number) {
+
+  for (var i=0; i<number; i++) {
+    var shape = shapes[Math.floor(Math.random()*shapes.length)];
+    playerGrid.unshift(shape);
+  }
+
+  for (var i = 0; i < gameGridsize; i++) {
+    $('#player-gridsquare-'+i).html(playerGrid[i]);
+  }
+  return playerGrid;
+}
+
+function playGame() {
+  var gridsquareSelected = $(this);
+  var playerSelected     = $('#player-gridsquare-5');
+
+  if (gridsquareSelected.html() === playerSelected.html()) {
+    gridsquareSelected.css('background', 'green');
+    playerScore++;
+    playerGrid.pop();
+    setupPlayerGrid(1);
+  } else {
+    gridsquareSelected.css('background', 'red');
+    playerScore--;
+  }
+  setTimeout(function(){ gridsquareSelected.css('background', 'none') }, 200);
+}
+
