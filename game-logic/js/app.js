4145350a3a5ee6aa28c6b81176a198befea661e8
benlayer291 <benlayer291@gmail.com>
2015-12-12 19:13:55 +0000

    Improved computer play function by adapting play game function. One problem is that the clicker cannot be identified, and thus at times scores can be attributed to both players

--- game-logic/js/app.js
+++ game-logic/js/app.js
@@ -7,8 +7,9 @@ function initialize(){
   timer();
   setupGameGrid();
   setupPlayerGrid(6);
+  setupCpuGrid(6);
   $('.game-gridsquare').on('click', playGame);
-  cpuPlayGame();
+  setTimeout(cpuPlayGame, 500);
 }
 
 // Variables
@@ -16,12 +17,14 @@ var shapes       = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
 var gameGridsize = Math.pow(3,2);
 var gameGrid     = [];
 var playerGrid   = [];
+var cpuGrid      = [];
 var playerScore  = 0;
+var cpuScore     = 0;
 var gameTime     = 30;
 
 // Timer function
 function timer() {
-  $('#timer').html('Time: ' + gameTime);
+  $('.timer').html('Time: ' + gameTime);
   var timeRemaining = setInterval(function(){
     if(gameTime > 0) {
       gameTime--;
@@ -29,11 +32,11 @@ function timer() {
       clearInterval(timeRemaining);
       $('.game-gridsquare').off('click');
     }
-    $('#timer').html('Time: ' + gameTime);
+    $('.timer').html('Time: ' + gameTime);
   }, 1000);
 }
 
-//Setup game grid and player grid
+//Setup game grid, player grid and cpu (player 2) grid;
 function setupGameGrid(){
   for (var i = 0; i < gameGridsize; i++) {
 
@@ -62,32 +65,63 @@ function setupPlayerGrid(number) {
   return playerGrid;
 }
 
+function setupCpuGrid(number) {
+
+  for (var i=0; i<number; i++) {
+    var shape = shapes[Math.floor(Math.random()*shapes.length)];
+    cpuGrid.unshift(shape);
+  }
+
+  for (var i = 0; i < gameGridsize; i++) {
+    $('#cpu-gridsquare-'+i).html(cpuGrid[i]);
+  }
+  return cpuGrid;
+}
+
 //Game play function upon square click
 function playGame() {
   var gridsquareSelected = $(this);
   var playerSelected     = $('#player-gridsquare-5');
+  var cpuSelected        = $('#cpu-gridsquare-5');
 
   if (gridsquareSelected.html() === playerSelected.html()) {
 
-    updateScore(gridsquareSelected, true);
+    updateScore('player', gridsquareSelected, true);
     changeGameGrid(gridsquareSelected);
     playerGrid.pop();
     setupPlayerGrid(1);
+
+  } else if (gridsquareSelected.html() === cpuSelected.html()) {
+
+    updateScore('cpu', gridsquareSelected, true);
+    changeGameGrid(gridsquareSelected);
+    cpuGrid.pop();
+    setupCpuGrid(1);
+
+  } else if (gridsquareSelected.html() !== cpuSelected.html()) {
+    updateScore('player', gridsquareSelected, false);
   } else {
-    updateScore(gridsquareSelected, false);
+    updateScore('cpu', gridsquareSelected, false);
   }
   setTimeout(function(){ gridsquareSelected.css('background', 'none') }, 200);
 }
 
 //Update the score of the player upon correct and incorrect choices
-function updateScore(gridsquareSelected, value) {
-  if (value === true) {
+function updateScore(player, gridsquareSelected, value) {
+  if (player === 'player' && value === true) {
     playerScore++;
-    gridsquareSelected.css('background', 'green');
     $('#player-score')
       .html('Score: '+playerScore)
       .css('color', 'green');
     setTimeout(function(){ $('#player-score').css('color', 'black') }, 200);
+    gridsquareSelected.css('background', 'green');
+  } else if (player === 'cpu' && value === true) {
+    cpuScore++;
+    $('#cpu-score')
+      .html('Score: '+cpuScore)
+      .css('color', 'green');
+    setTimeout(function(){ $('#cpu-score').css('color', 'black') }, 200);
+    gridsquareSelected.css('background', 'green');
   } else {
     if (playerScore > 0) playerScore--; 
 
@@ -114,19 +148,9 @@ function changeGameGrid(gridsquareSelected) {
 
 //CPU Player
 function cpuClickEvent() {
-  var cpuSelectedShape = $('#player-gridsquare-5').html();
+  var cpuSelectedShape = $('#cpu-gridsquare-5').html();
   var cpuSelectedShapeIndex = gameGrid.indexOf(cpuSelectedShape);
   return $('#'+cpuSelectedShapeIndex).trigger('click');
-}
-
-function cpuPlayGame() {
-  setTimeout(function(){
-    console.log(gameTime);
-    while (gameTime > 0) {
-      console.log(gameTime);
-      setInterval(cpuClickEvent(), 1000);
-    }
-  }, 1000);
 }
 
 function cpuPlayGame() {
@@ -137,7 +161,7 @@ function cpuPlayGame() {
   var cpuMove = setInterval(function(){
 
     if(gameTime > 0) {
-      setInterval(cpuClickEvent());
+      cpuClickEvent();
     } else {
       clearInterval(cpuPlayGame);
       $('.game-gridsquare').off('click');
