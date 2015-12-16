233ca1ea0e0ad1eb301b0a5203cead0608dc9ea0
benlayer291 <benlayer291@gmail.com>
2015-12-16 10:54:19 +0000

    scores create and shoe routes work, tested with insomnia

--- sockets-game/controllers/scoresController.js
+++ sockets-game/controllers/scoresController.js
@@ -1,3 +1,4 @@
+var User = require('../models/user');
 var Score = require('../models/score');
 
 function scoresIndex(req, res) {
@@ -17,6 +18,24 @@ function scoresShow(req, res) {
 }
 
 function scoresCreate(req, res) {
+  var userId = req.body.user_id;
+
+  User.findOne({_id: userId}, function(err, user){
+    if (err) return res.status(500).json({ message: 'Something went wrong.' });
+
+    var newScore = new Score(req.body);
+
+    newScore.save(function(err, score){
+      if (err) return res.status(500).json({ message: 'Something went wrong.' });
+
+      user.scores.push(score);
+
+      user.save(function(err, user){
+        if (err) return res.status(500).json({ message: 'Something went wrong.' });
+        return res.status(201).json({ message: 'Score succesfully created and added to user', score: score });  
+      });
+    });
+  });
   
 }
 
--- sockets-game/models/score.js
+++ sockets-game/models/score.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var scoreSchema = new mongoose.Schema({
-  value: Number
+  value: {type: Number, default: 0}
 })
 
 module.exports = mongoose.model('Score', scoreSchema);
