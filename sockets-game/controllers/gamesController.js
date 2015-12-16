261d26f0c930d25275ac34ea0a6fe9854481e373
benlayer291 <benlayer291@gmail.com>
2015-12-16 11:38:19 +0000

    games update and delete routes work, tested with insomnia

--- sockets-game/controllers/gamesController.js
+++ sockets-game/controllers/gamesController.js
@@ -27,11 +27,27 @@ function gamesCreate(req,res) {
 }
 
 function gamesUpdate(req,res) {
-  
+  var id = req.params.id;
+  Game.findOneAndUpdate({_id: id}, req.body, function(err, game){
+    if (err) return res.status(500).json({ message: 'Something went wrong.' });
+    if (!game) return res.status(404).json({ message: 'Game could not be found'});
+
+    if (req.body.game_id) game.game_id = req.body.game_id;
+
+    game.save(function(err){
+      if (err) return res.status(500).json({ message: 'Something went wrong.' });
+      return res.status(201).json({ message: 'game succesfully updated'});
+    });
+  });
 }
 
 function gamesDelete(req,res) {
-  
+  var id     = req.params.id;
+
+  Game.findOneAndRemove({_id: id}, function(err){
+    if (err) return res.status(500).json({ message: 'Something went wrong.' });
+    return res.status(201).json({ message: 'Game succesfully deleted'});
+  });
 }
 
 module.exports = {
