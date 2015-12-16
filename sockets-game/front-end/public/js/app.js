bcd7751358ff1192c8daff2320f1a0efa1d8b29e
benlayer291 <benlayer291@gmail.com>
2015-12-16 20:09:59 +0000

    commented out sockets work in front-end app.js so doesnt' repeat

--- sockets-game/front-end/public/js/app.js
+++ sockets-game/front-end/public/js/app.js
@@ -67,69 +67,69 @@ angular
 // // leave to unsubscribe the socket to a given channel (server-side)
 // socket.leave('some room');
 
-var socket = io.connect();
-var gameChannel;
+// var socket = io.connect();
+// var gameChannel;
 
-$(function(){
-  $("form").on("submit", start);
-  $(".game-list").on("click", ".join-game", join);
-});
+// $(function(){
+//   $("form").on("submit", start);
+//   $(".game-list").on("click", ".join-game", join);
+// });
 
-function start(){
-  event.preventDefault();
-  socket.emit('newGame', socket.io.engine.id);
-}
+// function start(){
+//   event.preventDefault();
+//   socket.emit('newGame', socket.io.engine.id);
+// }
 
-function join(){
-  event.preventDefault();
-  var gameId = $(this).data('gameid');
-  socket.emit('joinedGame', gameId, socket.io.engine.id)
-  return inGame(gameId)
-}
+// function join(){
+//   event.preventDefault();
+//   var gameId = $(this).data('gameid');
+//   socket.emit('joinedGame', gameId, socket.io.engine.id)
+//   return inGame(gameId)
+// }
 
-function inGame(gameid){
-  $(".game-list").hide();
-  $(".new-game-tools").hide();
-  return $(".message").html("<h2>You are playing in game: "+gameid+"</h2>")
-}
+// function inGame(gameid){
+//   $(".game-list").hide();
+//   $(".new-game-tools").hide();
+//   return $(".message").html("<h2>You are playing in game: "+gameid+"</h2>")
+// }
 
-function setUpPlayerShape(game) {
-  // Setup player-grid, different for each player- comes from client side
-  var socketId    = socket.io.engine.id;
-  var playerShape = game['main-grid'][(Math.floor(Math.random()*game['main-grid'].length))];
+// function setUpPlayerShape(game) {
+//   // Setup player-grid, different for each player- comes from client side
+//   var socketId    = socket.io.engine.id;
+//   var playerShape = game['main-grid'][(Math.floor(Math.random()*game['main-grid'].length))];
 
-  console.log(playerShape);
-  return $('#player-selected-gridsquare').html(playerShape);
-}
+//   console.log(playerShape);
+//   return $('#player-selected-gridsquare').html(playerShape);
+// }
 
-function playGame(){
-  var gameId        = $(this).data('gameid');
-  var socketId      = socket.io.engine.id;
-  var playerShape   = $('#player-selected-gridsquare').html();
-  var squareClicked = $(this).html();
-  socket.emit('playingGame', gameId, socketId, playerShape, squareClicked);
-}
+// function playGame(){
+//   var gameId        = $(this).data('gameid');
+//   var socketId      = socket.io.engine.id;
+//   var playerShape   = $('#player-selected-gridsquare').html();
+//   var squareClicked = $(this).html();
+//   socket.emit('playingGame', gameId, socketId, playerShape, squareClicked);
+// }
 
 
-function gameTimer() {
-  var gameTime = 30;
+// function gameTimer() {
+//   var gameTime = 30;
   
-  $('.timer').html('Time: ' + gameTime);
-  setTimeout(function(){ $('.notifications').empty() }, 1000)
-  var timeRemaining = setInterval(function(){
-    if(gameTime > 0) {
-      gameTime--;
-    } else {
-      $('.notifications').append('<li>Game Over!</li>');
-      $('.game-gridsquare').off('click');
-      clearInterval(timeRemaining);
-      // endGame(game);
-    }
-    $('.timer').html('Time: ' + gameTime);
-  }, 1000);
-  $('.game-gridsquare').on('click', playGame);
-  return gameTime;
-}
+//   $('.timer').html('Time: ' + gameTime);
+//   setTimeout(function(){ $('.notifications').empty() }, 1000)
+//   var timeRemaining = setInterval(function(){
+//     if(gameTime > 0) {
+//       gameTime--;
+//     } else {
+//       $('.notifications').append('<li>Game Over!</li>');
+//       $('.game-gridsquare').off('click');
+//       clearInterval(timeRemaining);
+//       // endGame(game);
+//     }
+//     $('.timer').html('Time: ' + gameTime);
+//   }, 1000);
+//   $('.game-gridsquare').on('click', playGame);
+//   return gameTime;
+// }
 
 // function endGame(game) {
 //   console.log(game);
@@ -161,75 +161,75 @@ function gameTimer() {
 //   }
 // }
 
-//SOCKET LISTENING EVENTS
-socket.on('connect', function(){
-  console.log('connected', socket.io.engine.id);
-});
+// //SOCKET LISTENING EVENTS
+// socket.on('connect', function(){
+//   console.log('connected', socket.io.engine.id);
+// });
 
-socket.on('addToListOfGames', function(game){
-  console.log("Game data received from socket.on:addToListOfGames ", game);
+// socket.on('addToListOfGames', function(game){
+//   console.log("Game data received from socket.on:addToListOfGames ", game);
 
-  if (socket.io.engine.id === game.id) {
-    return inGame(game.id)
-  } else {
-    return $(".game-list").append("<li>"+game.id+"<span>Players: "+game.players.length+"</span><a href='#' data-gameid='"+game.id+"' class='join-game'>Join</a></li>");
-  }
-})
+//   if (socket.io.engine.id === game.id) {
+//     return inGame(game.id)
+//   } else {
+//     return $(".game-list").append("<li>"+game.id+"<span>Players: "+game.players.length+"</span><a href='#' data-gameid='"+game.id+"' class='join-game'>Join</a></li>");
+//   }
+// })
 
-socket.on('start', function(game){
-  var countdownTime = 5;
+// socket.on('start', function(game){
+//   var countdownTime = 5;
 
-  // Countdown Timer
-  $('.notifications').append('<li>Get Ready!</li>');
-  $('.timer').html('Time: ' + countdownTime);
-  var countdownRemaining = setInterval(function(){
-    if(countdownTime > 0) {
-      countdownTime--;
-    } else {
-      $('.notifications')
-        .empty()
-        .append('<li>Go!</li>');
-      clearInterval(countdownRemaining);
-      gameTimer();
-    }
-    $('.timer').html('Time: ' + countdownTime);
-  }, 1000);
+//   // Countdown Timer
+//   $('.notifications').append('<li>Get Ready!</li>');
+//   $('.timer').html('Time: ' + countdownTime);
+//   var countdownRemaining = setInterval(function(){
+//     if(countdownTime > 0) {
+//       countdownTime--;
+//     } else {
+//       $('.notifications')
+//         .empty()
+//         .append('<li>Go!</li>');
+//       clearInterval(countdownRemaining);
+//       gameTimer();
+//     }
+//     $('.timer').html('Time: ' + countdownTime);
+//   }, 1000);
 
-  // Setup main-grid when timer finishes- same for both players, comes from server side
-  for (var i = 0; i < game['main-grid'].length; i++) {
-    $('#'+i)
-      .html(game['main-grid'][i])
-      .attr('data-gameid', game.id);
-  }
+//   // Setup main-grid when timer finishes- same for both players, comes from server side
+//   for (var i = 0; i < game['main-grid'].length; i++) {
+//     $('#'+i)
+//       .html(game['main-grid'][i])
+//       .attr('data-gameid', game.id);
+//   }
 
-  // Setup scores with player socket ids
-  for (var i = 0; i < game.players.length; i++) {
-    $('.score').append('<li id=score-'+game.players[i].id+'>Score:'+game.players[i].score+'</li>')
-  }
+//   // Setup scores with player socket ids
+//   for (var i = 0; i < game.players.length; i++) {
+//     $('.score').append('<li id=score-'+game.players[i].id+'>Score:'+game.players[i].score+'</li>')
+//   }
 
-  return setUpPlayerShape(game);
-});
+//   return setUpPlayerShape(game);
+// });
 
-socket.on('correctChoice', function(game, socketId){
+// socket.on('correctChoice', function(game, socketId){
 
-  // Update the main grid
-  for (var i = 0; i < game['main-grid'].length; i++) {
-    $('#'+i)
-    .html(game['main-grid'][i])
-    .attr('data-gameid', game.id);
-  }
+//   // Update the main grid
+//   for (var i = 0; i < game['main-grid'].length; i++) {
+//     $('#'+i)
+//     .html(game['main-grid'][i])
+//     .attr('data-gameid', game.id);
+//   }
 
-  // Player gets a new shape
-  if (socketId === socket.io.engine.id) {
-    var playerShape = game['main-grid'][(Math.floor(Math.random()*game['main-grid'].length))];
-    console.log(playerShape);
-    $('#player-selected-gridsquare').html(playerShape);
-  }
+//   // Player gets a new shape
+//   if (socketId === socket.io.engine.id) {
+//     var playerShape = game['main-grid'][(Math.floor(Math.random()*game['main-grid'].length))];
+//     console.log(playerShape);
+//     $('#player-selected-gridsquare').html(playerShape);
+//   }
 
-  // Player gets a point
-  console.log("SCORES: ", game.players);
-  for (var i = 0; i < game.players.length; i++) {
-    console.log(game.players[i]);
-    $('#score-'+game.players[i].id).html('Score: '+ game.players[i].score);
-  }
-})
+//   // Player gets a point
+//   console.log("SCORES: ", game.players);
+//   for (var i = 0; i < game.players.length; i++) {
+//     console.log(game.players[i]);
+//     $('#score-'+game.players[i].id).html('Score: '+ game.players[i].score);
+//   }
+// })
