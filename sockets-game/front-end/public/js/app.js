496f30a8305e40393625c39b5ff0a1c527117f72
benlayer291 <benlayer291@gmail.com>
2015-12-16 16:50:32 +0000

    Added state router into app.js

--- sockets-game/front-end/public/js/app.js
+++ sockets-game/front-end/public/js/app.js
@@ -1,9 +1,35 @@
 angular
   .module('shapes', ['ngResource', 'angular-jwt', 'ui.router'])
   .constant('API', 'http://localhost:3000/api')
+  .config(MainRouter)
   .config(function($httpProvider){
     $httpProvider.interceptors.push('authInterceptor')
   })
+
+  MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function MainRouter($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: "/",
+        templateUrl: "home.html"
+      })
+      .state('login', {
+        url: "/login",
+        templateUrl: "login.html"
+      })
+      .state('register', {
+        url: "/register",
+        templateUrl: "register.html"
+      })
+      .state('profile', {
+        url: "/profile",
+        templateUrl: "profile.html"
+      })
+
+    $urlRouterProvider.otherwise("/");
+  }
+
 // // sending to sender-client only
 // socket.emit('message', "this is a test");
 
