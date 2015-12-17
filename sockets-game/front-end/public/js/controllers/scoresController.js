a524fadb711d67b216ec24d64ca536b44a6101cb
benlayer291 <benlayer291@gmail.com>
2015-12-17 19:53:12 +0000

    Displaying users top 10 scores on profile page

---
+++ sockets-game/front-end/public/js/controllers/scoresController.js
@@ -0,0 +1,47 @@
+angular
+  .module('shapes')
+  .controller('ScoresController', ScoresController);
+
+ScoresController.$inject = ['Score', 'User', 'TokenService', 'CurrentUser']
+function ScoresController(Score, User, TokenService, CurrentUser) {
+
+  var self  = this;
+
+  self.all           = [];
+  self.player        = {}
+  self.oneUserScores = [];
+
+  self.getScores        = getScores;
+  self.getOneUserScores = getOneUserScores;
+
+  function getScores() {
+    Score.query(function(data){
+      self.all = data.scores;
+    })
+    console.log(self.all);
+  }
+
+  function getOneUserScores(user_id) {
+    Score.query(function(data){
+      console.log(data.scores[0].player);
+      console.log(self.player);
+      for (var i=0; i< data.scores.length; i++) {
+        if (data.scores[i].player === self.player._id) {
+          self.oneUserScores.push(data.scores[i]);
+        }
+      }
+    })
+    setTimeout(function(){
+      console.log(self.oneUserScores);
+    }, 1000);
+  }
+
+  if (CurrentUser.getUser()) {
+    self.player = TokenService.decodeToken();
+    console.log(self.player);
+  }
+
+  self.getOneUserScores();
+
+return self
+}
