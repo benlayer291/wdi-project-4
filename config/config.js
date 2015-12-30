fffbcd4ba1c0ccb60552bf5bfb4a830752ef9146
benlayer291 <benlayer291@gmail.com>
2015-12-30 15:00:45 +0000

    Added mongolab database uri

--- config/config.js
+++ config/config.js
@@ -1,4 +1,7 @@
+var mongolab = process.env.MONGOLAB_URI;
+
 module.exports = {
   'secret': 'shapesgamefinalproject',
-  'database': 'mongodb://localhost:27017/shapes'
+  // 'database': 'mongodb://localhost:27017/shapes'
+  'database': mongolab
 };
